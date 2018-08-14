791f1fe684c3beae24c251332822eae2cad3eaf5
Farid Kazemi <farid.kazemi@hsd.com.au>
2018-08-14 17:25:05 +1000

    First Commit

---
+++ ClientApp/src/app/administrator/administrator.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-administrator',
+  templateUrl: './administrator.component.html',
+})
+export class AdministratorComponent {
+}
